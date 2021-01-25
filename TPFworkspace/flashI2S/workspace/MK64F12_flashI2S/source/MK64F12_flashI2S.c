f52e459819edfb1d20fb06000bb252abab092deb
mlarroque <mlarroque@itba.edu.ar>
2021-01-25 14:33:01 -0300

    attach to project flash and starting SAI

---
+++ TPFworkspace/flashI2S/workspace/MK64F12_flashI2S/source/MK64F12_flashI2S.c
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2016-2021 NXP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of NXP Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file    MK64F12_flashI2S.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MK64F12.h"
+#include "fsl_debug_console.h"
+#include "flashHal.h"
+/* TODO: insert other include files here. */
+
+/* TODO: insert other definitions and declarations here. */
+#define DEBUG_FLASH_1 1
+#define DEBUG_SAI_1 0
+/*
+ * @brief   Application entry point.
+ */
+int main(void) {
+
+    /* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+    /* Init FSL debug console. */
+    BOARD_InitDebugConsole();
+#endif
+
+    PRINTF("Hello World\n");
+
+    /* Force the counter to be placed into memory. */
+    volatile static int i = 0 ;
+
+
+    /*start debugging*/
+
+#if DEBUG_FLASH_1
+    char data[] = {2, 4, 6, 8};  //array size: 5
+    char * dataTag = "prueba1";   //dataTagLen: 7
+    char * p2dataRead = 0;
+    int dataReadLen;
+    int j = 0;
+
+    if(flashINIT() != -1)
+    {
+    	PRINTF("flash init succesfuly\n");
+        if(flashAlloc((int *)data, sizeof(data[0])*4, dataTag, 7) != -1)
+        {
+        	PRINTF("flash alloc succesfuly\n");
+        	p2dataRead = readFlash(&dataReadLen, dataTag, 7);
+        	if( p2dataRead != 0 )
+        	{
+        		PRINTF("%p\n", p2dataRead);
+        		PRINTF("flash read succesfuly\n");
+        		for(j = 0; j < 5; j++)
+        		{
+        			printf("%d \n", p2dataRead[j]);
+        		}
+
+        	}
+        	else
+        	{
+        		PRINTF("READING FLASH ERROR\n");
+        	}
+        	}
+        else
+        {
+        	printf("WRITING FLASH ERROR\n");
+        }
+     }
+     else
+     {
+        printf("INITIALIZING FLASH ERROR\n");
+     }
+
+#elif DEBUG_SAI_1
+
+#endif /*DEBUG_FLASH_1, DEBUG_SAI_1*/
+
+    /* Enter an infinite loop, just incrementing a counter. */
+    while(1) {
+        i++ ;
+        /* 'Dummy' NOP to allow source level single stepping of
+            tight while() loop */
+        __asm volatile ("nop");
+    }
+    return 0 ;
+}
---
+++ TPFworkspace/flashI2S/workspace/MK64F12_flashI2S/source/flashHAL.c
@@ -0,0 +1,311 @@
+/*
+ * flashHAL.c
+ *
+ *  Created on: 30 mar. 2020
+ *      Author: G5
+ */
+
+#include "flashHAL.h"
+#include "fsl_ftfx_flash.h"
+#include <stdbool.h>
+
+#define MAX_STREAMS_ALLOWED 3
+#define STREAM_LEN 4096 * 2
+
+char stream[STREAM_LEN];
+
+uint32_t pflashBlockBase  = 0;
+uint32_t pflashTotalSize  = 0;
+uint32_t pflashSectorSize = 0;
+
+typedef struct
+{
+	char * tag;
+	int tagLen;
+	int dataLen;   //length in bytes
+	volatile uint8_t * startAdress;
+	_Bool isDataEmpty;
+}metaData_t;
+
+metaData_t dataStreams[MAX_STREAMS_ALLOWED];
+
+int streamIndex2program = 0;
+
+
+static flash_config_t flashConfig;
+
+status_t  flashStatus;
+
+bool isFlashINIT = false;
+
+void resetMetaData(uint32_t startAdress);
+
+_Bool isMetaDataValid(void);
+
+_Bool dataTagExists(char * dataTag, int dataTagLen, int * indexFound);
+
+void refreshIndex(void);
+
+void prepareStream(char * p2data, int dataLen);
+
+int flashINIT(void)
+{
+
+	int ret = 0;
+
+
+	if(!isFlashINIT)
+	{
+
+		/* Clean up Flash structure*/
+		memset(&flashConfig, 0, sizeof(flash_config_t));
+
+		flashStatus = FLASH_Init(&flashConfig);
+
+		/* Get flash properties*/
+		FLASH_GetProperty(&flashConfig, kFLASH_PropertyPflash0BlockBaseAddr, &pflashBlockBase);
+		FLASH_GetProperty(&flashConfig, kFLASH_PropertyPflash0TotalSize, &pflashTotalSize);
+		FLASH_GetProperty(&flashConfig, kFLASH_PropertyPflash0SectorSize, &pflashSectorSize);
+
+		flashConfig.ftfxConfig[0].ifrDesc.resRange.dflashIfrStart = pflashTotalSize - (MAX_STREAMS_ALLOWED* STREAM_LEN);
+		flashConfig.ftfxConfig[0].ifrDesc.resRange.pflashSwapIfrStart = pflashTotalSize - (MAX_STREAMS_ALLOWED* STREAM_LEN);
+		flashConfig.ftfxConfig[0].ifrDesc.resRange.ifrMemSize = MAX_STREAMS_ALLOWED*STREAM_LEN;
+	}
+	if(flashStatus != kStatus_FTFx_Success)
+	{
+		ret = -1;   //error detected
+	}
+	else
+	{
+		dataStreams[0].startAdress= (volatile uint8_t *)(pflashBlockBase + (pflashTotalSize - (MAX_STREAMS_ALLOWED * STREAM_LEN) ));
+		isFlashINIT = true;
+	}
+	return ret;
+}
+
+
+int flashAlloc(int * data, int dataLen, char * dataTag, int dataTagLen)
+{
+	int ret = 0;
+
+	if(isFlashINIT && (streamIndex2program < MAX_STREAMS_ALLOWED) && (dataLen > 0))
+	{
+		prepareStream((char *)data, dataLen);
+		flashStatus = FLASH_Erase(&flashConfig, (uint32_t) dataStreams[streamIndex2program].startAdress, STREAM_LEN, kFTFx_ApiEraseKey);
+		flashStatus = FLASH_Program( &flashConfig, (uint32_t) dataStreams[streamIndex2program].startAdress, (uint8_t *) stream, STREAM_LEN);
+
+		//refresh metaData
+		dataStreams[streamIndex2program].isDataEmpty = false;
+		dataStreams[streamIndex2program].dataLen = dataLen;
+		dataStreams[streamIndex2program].tagLen = (dataTagLen <= MAX_TAG_LEN)? dataTagLen : MAX_TAG_LEN;
+		dataStreams[streamIndex2program].tag = dataTag;
+		if(streamIndex2program < MAX_STREAMS_ALLOWED - 1)
+		{
+			streamIndex2program += 1;
+			dataStreams[streamIndex2program].startAdress = (volatile uint8_t *)(dataStreams[streamIndex2program - 1].startAdress + STREAM_LEN);
+		}
+
+		if(flashStatus != kStatus_FTFx_Success )
+		{
+			ret = -1;   //error detected
+		}
+	}
+	else
+	{
+		ret = -1;
+	}
+	return ret;
+}
+
+
+char *  readFlash(int * dataLen, char * dataTag, int dataTagLen)
+{
+	int indexFound;
+
+	if(dataTagExists(dataTag, dataTagLen, &indexFound))
+	{
+		dataLen[0] = STREAM_LEN;
+		memcpy(stream, dataStreams[indexFound].startAdress, STREAM_LEN);
+		return (volatile char *)stream;
+		//return (volatile char *)(dataStreams[indexFound].startAdress);
+
+		/*flashStatus = FLASH_ReadResource(&flashConfig, (uint32_t) (dataStreams[indexFound].startAdress), (uint8_t *) stream, (uint32_t) STREAM_LEN, kFTFx_ResourceOptionFlashIfr);
+		if(flashStatus != kStatus_FTFx_Success )
+		{
+			ret = -1;   //error detected
+		}*/
+		//memcpy((void *)stream, (void *)dataStreams[indexFound].startAdress, STREAM_LEN);
+	}
+	else
+	{
+		return 0; //error
+	}
+
+
+}
+
+
+
+
+/*void resetMetaData(uint32_t startAdress)
+{
+	int i = 0;
+	for(i = 0; i < MAX_STREAMS_ALLOWED; i++)
+	{
+		dataStreams[i].isDataEmpty = false;
+		dataStreams[i].startAdress = startAdress;
+		dataStreams[i].dataLen = 0;
+	}
+	indexFree = 0;
+}*/
+
+/*_Bool isMetaDataValid(void)
+{
+	return (dataStreams[0].startAdress == flashConfig.flashDesc.blockBase) &&
+			((dataStreams[MAX_STREAMS_ALLOWED - 1].startAdress  + dataStreams[MAX_STREAMS_ALLOWED - 1].dataLen)
+					== (flashConfig.flashDesc.blockBase + flashConfig.flashDesc.totalSize));
+}*/
+
+_Bool dataTagExists(char * dataTag, int dataTagLen, int * indexFound)
+{
+	int i = 0, j = 0;
+	_Bool wordMissmatched = false, tagFound = false;
+	if((dataTagLen > 0) && (dataTag != 0))
+	{
+		for(i = 0; (i < MAX_STREAMS_ALLOWED) && (!tagFound); i++)
+		{
+			if(!dataStreams[i].isDataEmpty && (dataStreams[i].tagLen == dataTagLen))
+			{
+				for(j = 0; (j < dataTagLen) && (j < MAX_TAG_LEN) && (!wordMissmatched); j++)
+				{
+					if ((dataStreams[i].tag)[j] != dataTag[j])
+					{
+						wordMissmatched = true; //wordMissmatched because of the difference between chars in dataTag
+					}
+				}
+			}
+			else
+			{
+				wordMissmatched = true; //wordMissmatched because of dataEmpy or 'len missmatched'
+			}
+			if(!wordMissmatched)
+			{
+				tagFound = true;
+				*indexFound = i;
+			}
+		}
+	}
+
+	return tagFound;
+}
+
+void prepareStream(char * p2data, int dataLen)
+{
+	memset(stream, 0, STREAM_LEN);
+	int i = 0;
+	if (dataLen > STREAM_LEN){
+		dataLen = STREAM_LEN;
+	}
+	for (i= 0; i < dataLen; i++){
+		stream[i] = p2data[i];
+	}
+}
+
+void refreshIndex(void)
+{/*
+	streamIndex2program++;
+	if(indexFree >= MAX_STREAMS_ALLOWED)
+	{
+		indexFree = 0;
+	}*/
+}
+/*@brief Erases the flash sectors encompassed by parameters passed into function.
+ *
+ * This function erases the appropriate number of flash sectors based on the
+ * desired start address and length.
+ *
+ * @param config The pointer to the storage for the driver runtime state.
+ * @param start The start address of the desired flash memory to be erased.
+ *              The start address does not need to be sector-aligned but must be word-aligned.
+ * @param lengthInBytes The length, given in bytes (not words or long-words)
+ *                      to be erased. Must be word-aligned.
+ * @param key The value used to validate all flash erase APIs.
+ *
+ * @retval #kStatus_FTFx_Success API was executed successfully.
+ * @retval #kStatus_FTFx_InvalidArgument An invalid argument is provided.
+ * @retval #kStatus_FTFx_AlignmentError The parameter is not aligned with the specified baseline.
+ * @retval #kStatus_FTFx_AddressError The address is out of range.
+ * @retval #kStatus_FTFx_EraseKeyError The API erase key is invalid.
+ * @retval #kStatus_FTFx_ExecuteInRamFunctionNotReady Execute-in-RAM function is not available.
+ * @retval #kStatus_FTFx_AccessError Invalid instruction codes and out-of bounds addresses.
+ * @retval #kStatus_FTFx_ProtectionViolation The program/erase operation is requested to execute on protected areas.
+ * @retval #kStatus_FTFx_CommandFailure Run-time error during the command execution.
+ */
+//status_t FTFx_CMD_Erase(ftfx_config_t *config,
+ //                       uint32_t start,
+ //                       uint32_t lengthInBytes,
+ //                       uint32_t key);
+
+
+
+
+/*@brief Programs flash with data at locations passed in through parameters.
+*
+* This function programs the flash memory with the desired data for a given
+* flash area as determined by the start address and the length.
+*
+* @param config A pointer to the storage for the driver runtime state.
+* @param start The start address of the desired flash memory to be programmed. Must be
+*              word-aligned.
+* @param src A pointer to the source buffer of data that is to be programmed
+*            into the flash.
+* @param lengthInBytes The length, given in bytes (not words or long-words),
+*                      to be programmed. Must be word-aligned.
+*
+* @retval #kStatus_FTFx_Success API was executed successfully.
+* @retval #kStatus_FTFx_InvalidArgument An invalid argument is provided.
+* @retval #kStatus_FTFx_AlignmentError Parameter is not aligned with the specified baseline.
+* @retval #kStatus_FTFx_AddressError Address is out of range.
+* @retval #kStatus_FTFx_ExecuteInRamFunctionNotReady Execute-in-RAM function is not available.
+* @retval #kStatus_FTFx_AccessError Invalid instruction codes and out-of bounds addresses.
+* @retval #kStatus_FTFx_ProtectionViolation The program/erase operation is requested to execute on protected areas.
+* @retval #kStatus_FTFx_CommandFailure Run-time error during the command execution.
+*/
+//status_t FTFx_CMD_Program(ftfx_config_t *config,
+ //                        uint32_t start,
+  //                       uint8_t *src,
+   //                      uint32_t lengthInBytes);
+
+
+/*!
+ * @brief Reads the resource with data at locations passed in through parameters.
+ *
+ * This function reads the flash memory with the desired location for a given
+ * flash area as determined by the start address and length.
+ *
+ * @param config A pointer to the storage for the driver runtime state.
+ * @param start The start address of the desired flash memory to be programmed. Must be
+ *              word-aligned.
+ * @param dst A pointer to the destination buffer of data that is used to store
+ *        data to be read.
+ * @param lengthInBytes The length, given in bytes (not words or long-words),
+ *        to be read. Must be word-aligned.
+ * @param option The resource option which indicates which area should be read back.
+ *
+ * @retval #kStatus_FTFx_Success API was executed successfully.
+ * @retval #kStatus_FTFx_InvalidArgument An invalid argument is provided.
+ * @retval #kStatus_FTFx_AlignmentError Parameter is not aligned with the specified baseline.
+ * @retval #kStatus_FTFx_ExecuteInRamFunctionNotReady Execute-in-RAM function is not available.
+ * @retval #kStatus_FTFx_AccessError Invalid instruction codes and out-of bounds addresses.
+ * @retval #kStatus_FTFx_ProtectionViolation The program/erase operation is requested to execute on protected areas.
+ * @retval #kStatus_FTFx_CommandFailure Run-time error during the command execution.
+ */
+//#if defined(FSL_FEATURE_FLASH_HAS_READ_RESOURCE_CMD) && FSL_FEATURE_FLASH_HAS_READ_RESOURCE_CMD
+//status_t FTFx_CMD_ReadResource(ftfx_config_t *config,
+ //                              uint32_t start,
+  //                             uint8_t *dst,
+   //                            uint32_t lengthInBytes,
+    //                           ftfx_read_resource_opt_t option);
+//#endif
+
+
